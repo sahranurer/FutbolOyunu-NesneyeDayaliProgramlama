542eb9910e2f306c007275d6d9a2be4fc3ec5b01
Sahranur Er <sahranurerr@gmail.com>
2021-08-07 17:22:47 +0300

    Add project files.

---
+++ FutbolOyunu/Futbol.cs
@@ -0,0 +1,196 @@
+﻿using FutbolOyunu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutbolOyunu
+{
+    class Futbol
+    {
+        public  Random rastgeleSayi = new Random();
+        protected string adSoyad;
+        protected int formaNo;
+        protected int hiz;
+        protected int dayaniklik;
+        protected int pas;
+        protected int sut;
+        protected int yetenek;
+        protected int kararlilik;
+        protected int dogalForm;
+        protected int sans;
+        public Futbol(string adSoyad, int formaNo)
+        {
+            hiz = rastgeleSayi.Next(50, 100);
+            dayaniklik = rastgeleSayi.Next(50,100);
+            pas = rastgeleSayi.Next(50,100);
+            sut = rastgeleSayi.Next(50,100);
+            yetenek = rastgeleSayi.Next(50,100);
+            kararlilik = rastgeleSayi.Next(50,100);
+            dogalForm = rastgeleSayi.Next(50,100);
+            sans = rastgeleSayi.Next(50,100);
+    }
+        public Futbol()
+        {
+
+        }
+        public virtual bool passVer()
+        {
+            int PasSkor = Convert.ToInt32(pas * 0.3 + yetenek * 0.3 + dayaniklik * 0.1 + dogalForm * 0.1 + sans * 0.2);
+            if (PasSkor>60)
+            {
+                return true;
+            }
+            else
+              return false;
+        }
+        public virtual bool golVurusu()
+        {
+            int golSkor = Convert.ToInt32(yetenek * 0.3 + sut * 0.2 + kararlilik * 0.1 + dogalForm * 0.1 + sans * 0.2 + hiz*0.1 );
+            if (golSkor > 70)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+
+    }
+    class Defans : Futbol
+    {
+        private int pozisyonAlma;
+        private int kafa;
+        private int sicrama;
+        public Defans(string adSoyad, int formaNo)
+        {
+            this.adSoyad = adSoyad;
+            this.formaNo = formaNo;
+            hiz = rastgeleSayi.Next(50, 90);
+            dayaniklik = rastgeleSayi.Next(50, 90);
+            pas = rastgeleSayi.Next(50, 90);
+            sut = rastgeleSayi.Next(50, 90);
+            yetenek = rastgeleSayi.Next(50, 90);
+            kararlilik = rastgeleSayi.Next(50, 90);
+            dogalForm = rastgeleSayi.Next(50, 90);
+            sans = rastgeleSayi.Next(50, 90);
+            pozisyonAlma = rastgeleSayi.Next(50, 90);
+            kafa = rastgeleSayi.Next(50, 90);
+            sicrama = rastgeleSayi.Next(50, 90);
+        }
+
+        public virtual bool golVurusu()
+        {
+            int golSkor = Convert.ToInt32(yetenek * 0.3 + sut * 0.2 + kararlilik * 0.1 + dogalForm * 0.1 + sans * 0.2 + kafa * 0.1+kararlilik*0.1);
+            if (golSkor > 70)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+    }
+    class OrtaSaha : Futbol
+    {
+        private int uzunTop;
+        private int ilkDokunus;
+        private int uretkenlik;
+        private int topSurme;
+        private int ozelYetenek;
+        public OrtaSaha(string adSoyad, int formaNo)
+        {
+            this.adSoyad = adSoyad;
+            this.formaNo = formaNo;
+            hiz = rastgeleSayi.Next(60, 100);
+            dayaniklik = rastgeleSayi.Next(60, 100);
+            pas = rastgeleSayi.Next(60, 100);
+            sut = rastgeleSayi.Next(60, 100);
+            yetenek = rastgeleSayi.Next(60, 100);
+            kararlilik = rastgeleSayi.Next(60, 100);
+            dogalForm = rastgeleSayi.Next(60, 100);
+            sans = rastgeleSayi.Next(60, 100);
+            uzunTop = rastgeleSayi.Next(60, 100);
+            ilkDokunus = rastgeleSayi.Next(60, 100);
+            uretkenlik = rastgeleSayi.Next(60, 100);
+            topSurme = rastgeleSayi.Next(60, 100);
+            ozelYetenek = rastgeleSayi.Next(60, 100);
+        }
+
+        public override bool passVer()
+        {
+            int PasSkor = Convert.ToInt32(pas * 0.3 + dayaniklik * 0.1 + dogalForm * 0.1 + sans * 0.2);
+            if (PasSkor > 60)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public virtual bool golVurusu()
+        {
+            int golSkor = Convert.ToInt32(yetenek * 0.3 + sut*0.2 +kararlilik*0.1+ dogalForm * 0.1 + sans * 0.2+hiz*0.1);
+            if (golSkor > 70)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+    class Forvet : Futbol
+    {
+        private int bitiricilik;
+        private int ilkDokunus;
+        private int kafa;
+        private int ozelYetenek;
+        private int sogukKanlilik;
+        public Forvet(string adSoyad, int formaNo)
+        {
+            this.adSoyad = adSoyad;
+            this.formaNo = formaNo;
+            hiz = rastgeleSayi.Next(70, 100);
+            dayaniklik = rastgeleSayi.Next(70, 100);
+            pas = rastgeleSayi.Next(70, 100);
+            sut = rastgeleSayi.Next(70, 100);
+            yetenek = rastgeleSayi.Next(70, 100);
+            kararlilik = rastgeleSayi.Next(70, 100);
+            dogalForm = rastgeleSayi.Next(70, 100);
+            sans = rastgeleSayi.Next(70, 100);
+            bitiricilik = rastgeleSayi.Next(70, 100);
+            ilkDokunus = rastgeleSayi.Next(70, 100);
+            kafa = rastgeleSayi.Next(70, 100);
+            ozelYetenek = rastgeleSayi.Next(70, 100);
+            sogukKanlilik = rastgeleSayi.Next(70, 100);
+        }
+
+        public override bool passVer()
+        {
+            int PasSkor = Convert.ToInt32(pas * 0.3 + yetenek * 0.2+ozelYetenek*0.2+ dayaniklik * 0.1 + dogalForm * 0.1 + sans * 0.2);
+            if (PasSkor > 60)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public virtual bool golVurusu()
+        {
+            int golSkor = Convert.ToInt32(yetenek * 0.2 + sut * 0.1 +kafa*0.1+ kararlilik * 0.1 + dogalForm * 0.1 + sans * 0.2 + kafa * 0.1 + kararlilik * 0.1);
+            if (golSkor > 70)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+    }
+}
+    
+   
+
---
+++ FutbolOyunu/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutbolOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Futbol> takim = new List<Futbol>();
+            takim.Add(new Futbol("Kaleci",1));
+            takim.Add(new Defans("defans1", 2));
+            takim.Add(new Defans("defans2", 3));
+            takim.Add(new Defans("defans3", 4));
+            takim.Add(new Defans("defans4", 5));
+            takim.Add(new OrtaSaha("ortasaha1", 6));
+            takim.Add(new OrtaSaha("ortasaha2", 7));
+            takim.Add(new OrtaSaha("ortasaha3", 8));
+            takim.Add(new OrtaSaha("ortasaha4", 9));
+            takim.Add(new Forvet("forvet1", 10));
+            takim.Add(new Forvet("forvet2", 11));
+            Random rastgele = new Random();
+            int formaNo;
+            int pasKontrol=12;
+            formaNo = rastgele.Next(1, 12);
+            Boolean golOlabilir = true;
+            for (int i = 0; i < 3; i++)
+            {
+                while (pasKontrol == formaNo)
+                {
+                    formaNo = rastgele.Next(1, 12);
+                }
+                pasKontrol = formaNo;
+                
+                if (!takim[formaNo-1].passVer())
+                {
+                    Console.WriteLine(formaNo + "Numaralı Oyuncunun Pası Başarısız.");
+                    golOlabilir = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(formaNo + "Numaralı Oyuncunun Pası Başarılı.");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+            Console.WriteLine(formaNo + "Numaralı kaleciyle karşı karşıya");
+            System.Threading.Thread.Sleep(1000);
+            if (golOlabilir = true)
+            {
+                if (takim[formaNo-1].golVurusu())
+                {
+                    Console.WriteLine(formaNo + "GOLLLLLLLLLLL !...");
+                }
+                else
+                {
+                    Console.WriteLine( "Az farkla Kaçırdı !...");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
